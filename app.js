9909556d9ad3986bb547fc43e05310cd40123a28
thuanntsolution <thuan.nguyenthanh.contact@gmail.com>
2025-02-12 22:45:48 +0700

    ✅ implement  connect to database

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ import { PORT } from "./config/env.js";
 import authRouter from "./routes/auth.routes.js";
 import userRouter from "./routes/user.routes.js";
 import subscriptionRouter from "./routes/subscription.routes.js";
+import connectToDatabase from "./database/mongodb.js";
 
 const app = express();
 
@@ -16,8 +17,10 @@ app.get("/", (req, res) => {
   res.send("Welcome to Subscription Tracker");
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, async () => {
   console.log(`Subcription Tracker is running on port ${PORT}`);
+
+  await connectToDatabase()
 });
 
 export default app;
