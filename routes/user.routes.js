7214f3774f38439f2ed2ba9811dbcdef3f084493
thuanntsolution <thuan.nguyenthanh.contact@gmail.com>
2025-02-14 22:52:31 +0700

    implement auth and user controller

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,9 +1,11 @@
 import {Router} from "express";
+import authorize from "../middlewares/auth.middleware.js";
+import {getUsers, getUser} from "../controllers/user.controller.js";
 
 const userRouter = Router()
 
-userRouter.get("/", (req, res) => res.send({title: "Get all users"}))
-userRouter.get("/:id", (req, res) => res.send({title: "Get user details"}))
+userRouter.get("/", getUsers)
+userRouter.get("/:id", authorize, getUser) 
 userRouter.post("/", (req, res) => res.send({title: "Create new user"}))
 userRouter.put("/:id", (req, res) => res.send({title: "Update user"}))
 userRouter.delete("/", (req, res) => res.send({title: "Delete user"}))
